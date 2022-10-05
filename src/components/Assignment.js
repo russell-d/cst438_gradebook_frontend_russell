9f85756b21f00d5964814d55cbf28f4dcda3d27d
russell-d <rdevera@csumb.edu>
2022-10-04 20:31:55 -0700

    func

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -83,6 +83,8 @@ class Assignment extends React.Component {
                     variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
               Grade
             </Button>
+            <Button component={Link} to={{pathname:'/gradebook/add', assignment: assignmentSelected}}
+                variant="outlined" color="primary"  style={{margin: 10}}>Add Assignment</Button>
             <ToastContainer autoClose={1500} /> 
           </div>
       )
